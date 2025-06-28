e1b98e73f18df39603f12be31b98d2bef5820692
V Shree Harshitha <vsh9945@gmail.com>
2025-06-28 22:45:20 +0530

    Database updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
 load_dotenv()
 import os
 
@@ -77,10 +79,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
 }
 
 # API Keys - Add these to your environment variables
